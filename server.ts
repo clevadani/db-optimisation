cfaae58e708693c8b2c896b1e72d9ecfa28a5723
Daniel <clevadani@gmail.com>
2022-04-06 00:11:07 +0100

    Updated script to accept arguments

--- server.ts
+++ server.ts
@@ -1,60 +1,82 @@
-const pgPromise = require('pg-promise');
-const R         = require('ramda');
-const request   = require('request-promise');
+const pgPromise = require("pg-promise");
+const R = require("ramda");
+const request = require("request-promise");
 
 // Limit the amount of debugging of SQL expressions
-const trimLogsSize : number = 200;
+const trimLogsSize: number = 200;
 
 // Database interface
-interface DBOptions
-  { host      : string
-  , database  : string
-  , user?     : string
-  , password? : string
-  , port?     : number
-  };
+interface DBOptions {
+  host: string;
+  database: string;
+  user?: string;
+  password?: string;
+  port?: number;
+}
 
 // Actual database options
-const options : DBOptions = {
-  // user: ,
-  // password: ,
-  host: 'localhost',
-  database: 'lovelystay_test',
+const options: DBOptions = {
+  host: "localhost",
+  database: "lovelystay_test",
 };
 
-console.info('Connecting to the database:',
-  `${options.user}@${options.host}:${options.port}/${options.database}`);
+console.info(
+  "Connecting to the database:",
+  `${options.user}@${options.host}:${options.port}/${options.database}`
+);
 
 const pgpDefaultConfig = {
-  promiseLib: require('bluebird'),
+  promiseLib: require("bluebird"),
   // Log all querys
   query(query) {
-    console.log('[SQL   ]', R.take(trimLogsSize,query.query));
+    console.log("[SQL   ]", R.take(trimLogsSize, query.query));
   },
   // On error, please show me the SQL
   error(err, e) {
-    if (e.query) {
-      console.error('[SQL   ]', R.take(trimLogsSize,e.query),err);
+    if (e.cn) {
+      console.error("[CONN ERROR   ]", R.take(trimLogsSize, e.cn), err);
+    } else if (e.query) {
+      console.error("[SQL ERROR  ]", R.take(trimLogsSize, e.query), err);
+    } else {
+      console.error("[ERROR   ]", R.take(trimLogsSize, e.message), err);
     }
-  }
+  },
+  connect(client) {
+    const cp = client.connectionParameters;
+    console.log("Connected to database:", cp.database);
+  },
 };
 
-interface GithubUsers
-  { id : number
-  };
+interface GithubUsers {
+  id: number;
+}
 
 const pgp = pgPromise(pgpDefaultConfig);
 const db = pgp(options);
-
-db.none('CREATE TABLE github_users (id BIGSERIAL, login TEXT, name TEXT, company TEXT)')
-.then(() => request({
-  uri: 'https://api.github.com/users/gaearon',
-  headers: {
-        'User-Agent': 'Request-Promise'
-    },
-  json: true
-}))
-.then((data: GithubUsers) => db.one(
-  'INSERT INTO github_users (login) VALUES ($[login]) RETURNING id', data)
-).then(({id}) => console.log(id))
-.then(() => process.exit(0));
+db.one(
+  "SELECT EXISTS ( SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename  = 'github_users' )"
+)
+  .then(
+    ({ exists }) =>
+      !exists &&
+      db.none(
+        "CREATE TABLE github_users (id BIGSERIAL, login TEXT, name TEXT, company TEXT)"
+      )
+  )
+  .then(() => {
+    return request({
+      uri: `https://api.github.com/users/${process.env.USER_NAME}`,
+      headers: {
+        "User-Agent": "Request-Promise",
+      },
+      json: true,
+    });
+  })
+  .then((data: GithubUsers) =>
+    db.one(
+      "INSERT INTO github_users (login) VALUES ($[login]) RETURNING id",
+      data
+    )
+  )
+  .then(({ id }) => console.log(id))
+  .then(() => process.exit(0));
