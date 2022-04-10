b73f366c97481394462e904165732c569d3c84bc
Daniel <clevadani@gmail.com>
2022-04-11 00:45:34 +0100

    Breaking up functions

--- server.ts
+++ server.ts
@@ -55,37 +55,47 @@ interface GithubUsers {
 
 const pgp = pgPromise(pgpDefaultConfig);
 const db = pgp(options);
-db.one(
-  "SELECT EXISTS ( SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename  = 'github_users' )"
-)
-  .then(
-    ({ exists }) =>
-      !exists &&
-      db.none(
-        "CREATE TABLE github_users (id BIGSERIAL, login TEXT UNIQUE, name TEXT, company TEXT, location TEXT)"
-      )
-  )
+
+const checkIfTableExists = database =>
+  database.one(
+    "SELECT EXISTS ( SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename  = 'github_users' )"
+  );
+
+const createTable = database =>
+  database.none(
+    "CREATE TABLE github_users (id BIGSERIAL, login TEXT UNIQUE, name TEXT, company TEXT, location TEXT)"
+  );
+
+const getGithubUser = (userName: string) =>
+  request({
+    uri: `https://api.github.com/users/${userName}`,
+    headers: {
+      "User-Agent": "Request-Promise",
+    },
+    json: true,
+  });
+
+const createUser = (database, data: GithubUsers) =>
+  database.one(
+    "INSERT INTO github_users (login, location) VALUES ($[login],$[location]) RETURNING id",
+    data
+  );
+
+const getAndCreateUser = (database, userName: string) =>
+  getGithubUser(userName)
+    .then((data: GithubUsers) => createUser(database, data))
+    .then(({ id }) => console.log(id));
+
+const getLisbonUsers = database =>
+  database.many(`SELECT * FROM github_users WHERE location = 'lisbon'`);
+
+checkIfTableExists(db)
+  .then(({ exists }) => !exists && createTable(db))
   .then(() => {
-    const location = process.env.LOCATION;
-    let query = `SELECT * FROM github_users`;
-    if (location) query += ` WHERE location = '${location}'`;
-    return db.many(query);
+    const userName: string = process.env.USER_NAME;
+    const shouldGetOrCreate = userName ? getAndCreateUser : getLisbonUsers;
+    return shouldGetOrCreate(db, userName);
   })
-  .then(res => console.log("res", res))
-  // .then(() => {
-  //   return request({
-  //     uri: `https://api.github.com/users/${process.env.USER_NAME}`,
-  //     headers: {
-  //       "User-Agent": "Request-Promise",
-  //     },
-  //     json: true,
-  //   });
-  // })
-  // .then((data: GithubUsers) => {
-  //   return db.one(
-  //     "INSERT INTO github_users (login, location) VALUES ($[login],$[location]) RETURNING id",
-  //     data
-  //   );
-  // })
-  // .then(({ id }) => console.log(id))
-  .then(() => process.exit(0));
+  .then(data => console.log(data))
+  .then(() => process.exit(0))
+  .catch(error => console.error(error));
